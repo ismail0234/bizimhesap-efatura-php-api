beae3820d85f45fc6beabb33673ad69b4cbf8144
İsmail <ismaiil_0234@hotmail.com>
2019-06-14 13:19:42 +0300

    Update HttpRequest.php

--- BizimHesap/HttpRequest.php
+++ BizimHesap/HttpRequest.php
@@ -17,10 +17,10 @@ Class HttpRequest
 
 		if (isset($options['data'])) {
 			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($options['data']));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options['data']));
 		}
 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
 		curl_setopt($ch, CURLOPT_HEADER, false);
   
